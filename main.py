91cf6a98f886502794c77533f5ac8706c608fe98
AldousSinen <aldous.sinen@fortress-asya.com>
2025-02-26 09:19:23 +0800

    update image in main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ st.title("THIS IS THE GREATEST SHOW")
 st.subheader("It is always fun to be here!")
 st.write("Join us and earn great salary!")
 st.divider()
-st.image("371461879_795595179066361_7370022226711550654_n.jpg", width=500, caption="this is roldan")
+st.image("Python.png", width=500, caption="this is roldan")
 
 
 
